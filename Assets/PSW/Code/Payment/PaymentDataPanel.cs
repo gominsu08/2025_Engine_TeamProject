f276098a7461911229dcbc512881ee593c51f167
pig-08 <pswhoonsmt@gmail.com>
2025-10-23 09:56:43 +0900

    faet : SystemPanelPrefab

--- Assets/PSW/Code/Payment/PaymentDataPanel.cs
+++ Assets/PSW/Code/Payment/PaymentDataPanel.cs
@@ -8,22 +8,27 @@ namespace PSW.Code.Payment
 {
     public class PaymentDataPanel : MonoBehaviour
     {
+        [SerializeField] private ResourceContainer resourceContainer;
         [SerializeField] private TextMeshProUGUI _coinText;
         [SerializeField] private int oneDayPlusCoin = 1000;
+
+        [SerializeField] private Color notTargetCoinColor;
         private int _dDayPaymentCoin;
 
         private bool _isNotPayment;
 
         private void Start()
         {
-            _coinText.color = Color.red;
             Bus<PaymentTimeEvent>.OnEvent += StartPayment;
             Bus<OneDayTimeEvent>.OnEvent += OneDay;
+            Bus<ChangeCoinEvent>.OnEvent += ChangeCoin;
         }
 
         private void StartPayment(PaymentTimeEvent evt)
         {
             _dDayPaymentCoin += oneDayPlusCoin;
+            SetTargetCoinText();
+            _coinText.text = _dDayPaymentCoin.ToString();
             _isNotPayment = true;
         }
 
@@ -39,10 +44,17 @@ namespace PSW.Code.Payment
 
         private void ChangeCoin(ChangeCoinEvent evt)
         {
-
+            SetTargetCoinText();
         }
 
+        private void SetTargetCoinText()
+        {
+            if (resourceContainer.IsTargetCoin(_dDayPaymentCoin))
+                _coinText.color = Color.white;
+            else
+                _coinText.color = notTargetCoinColor;
 
+        }
     }
 
     public struct GameOverEvent : IEvent
